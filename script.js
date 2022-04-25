13a5656c2eccb5c402432435ea876b1335b2da9b
NAGUiB-1 <91795418+NAGUiB-1@users.noreply.github.com>
2022-04-25 19:25:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let rightside = document.querySelector('.rightside')
+let toggle = document.querySelector('.toggle')
+
+toggle.addEventListener('click',(e)=> {
+ rightside.classList.toggle('show')
+})
