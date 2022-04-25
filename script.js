a1c82f16f135ef00c5359250ce6a10f7581a55ae
NAGUiB-1 <91795418+NAGUiB-1@users.noreply.github.com>
2022-04-26 01:25:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let rightside = document.querySelector('.rightside')
 let toggle = document.querySelector('.toggle')
+let likeList = document.querySelectorAll('.like')
 
 toggle.addEventListener('click',(e)=> {
  rightside.classList.toggle('show')
+})
+
+likeList.forEach((e)=>{
+  e.addEventListener('click', ()=>{
+    e.classList.toggle('liked')
+    console.log('cli')
+  })
 })
